2a28b3ef23d65ef50c20a1ce1efe81897abb8cdf
Colo <paolino.federico@gmail.com>
2016-08-29 11:57:50 -0300

    7ma Iteracion, quito warnings de los test ,falta lo de las lineas de codigo en la cola

--- src/test/java/ar/fiuba/tdd/template/MainTests.java
+++ src/test/java/ar/fiuba/tdd/template/MainTests.java
@@ -6,22 +6,26 @@ import static org.junit.Assert.*;
 
 public class MainTests {
 
+    Integer num5 = 5;
+    Integer num6 = 6;
+    Integer num7 = 7;
+
     @Test
     public void puedoCrearUnaCola() {
-        Cola cola1 = new Cola<Integer>();
+        Cola<Integer> cola1 = new Cola<Integer>();
         assertNotNull(cola1);
     }
 
     @Test
     public void puedoAgregarYSacarUnObjetoDeLaCola() {
-        Cola cola1 = new Cola<Integer>();
-        cola1.add(5);
-        cola1.add(6);
-        cola1.add(8);
+        Cola<Integer> cola1 = new Cola<Integer>();
+        cola1.add(num5);
+        cola1.add(num6);
+        cola1.add(num7);
         try {
-            assertEquals(cola1.top(), 5);
-            assertEquals(cola1.top(), 6);
-            assertEquals(cola1.top(), 8);
+            assertEquals(cola1.top(), num5);
+            assertEquals(cola1.top(), num6);
+            assertEquals(cola1.top(), num7);
         } catch (Exception exeption) {
             assertTrue(false);//falla el test si lanza la excepcion
         }
@@ -29,23 +33,33 @@ public class MainTests {
 
     @Test
     public void colaEstaVacia() {
-        Cola cola1 = new Cola<Integer>();
+        Cola<Integer> cola1 = new Cola<Integer>();
         assertTrue(cola1.isEmpty());
     }
 
     @Test
     public void colaNoEstaVacia() {
-        Cola cola1 = new Cola<Integer>();
-        cola1.add(1);
+        Cola<String> cola1 = new Cola<String>();
+        cola1.add("HOLA");
         assertFalse(cola1.isEmpty());
+        assertEquals("HOLA", cola1.top());
+    }
+
+    @Test
+    public void metoDiferentesTiposDeDatos() {
+        Cola<Object> cola1 = new Cola<Object>();
+        cola1.add("HOLA");
+        cola1.add(num5);
+        assertEquals("HOLA", cola1.top());
+        assertEquals(num5, cola1.top());
     }
 
     @Test
     public void puedoObtenerUnTamanioCorrecto() {
-        Cola cola1 = new Cola<Integer>();
-        cola1.add(5);
-        cola1.add(6);
-        cola1.add(7);
+        Cola<String> cola1 = new Cola<String>();
+        cola1.add("Hello");
+        cola1.add("Darkness");
+        cola1.add("My old friend");
         assertEquals(cola1.size(), 3);
         try {
             cola1.remove();
@@ -59,9 +73,9 @@ public class MainTests {
 
     @Test
     public void noPuedoSacarUnObjetoDeUnaColaVacia() {
-        Cola cola1 = new Cola<Integer>();
-        cola1.add(5);
-        cola1.add(6);
+        Cola<Integer> cola1 = new Cola<Integer>();
+        cola1.add(num5);
+        cola1.add(num6);
         try {
             cola1.remove();
             cola1.remove();
